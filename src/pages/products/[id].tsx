f7a89487f040855f2235c4fb2e86b64c2e27c85f
zubetcha <zuhye5@gmail.com>
2022-03-09 20:44:54 +0900

    commit test

---
+++ src/pages/products/[id].tsx
@@ -0,0 +1,3 @@
+const ProductDetail = () => <div>상품 상세</div>
+
+export default ProductDetail
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 
 const DynamicIndex = React.lazy(() => import('./pages/index'));
+const DynamicProductsId = React.lazy(() => import('./pages/products/[id]'));
 
 
 export const routes = [
@@ -11,10 +12,12 @@ export const routes = [
     element: <Outlet />,
     children: [
       { path: '/', element: <DynamicIndex />, index: true},
+      { path: '/products/:id', element: <DynamicProductsId />, },
     ]
   }
 ]
 
 export const pages = [
   { route: '/' },
+  { route: '/products/:id' },
 ]
