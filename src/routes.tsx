db758d4498b1b17fad16bf139092bbc34ce4bade
zubetcha <zuhye5@gmail.com>
2022-03-12 22:37:28 +0900

    Feat: create gnb

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import GlobalLayout from './pages/_layout'
 
 const DynamicIndex = React.lazy(() => import('./pages/index'));
+const DynamicCartIndex = React.lazy(() => import('./pages/cart/index'));
+const DynamicPaymentIndex = React.lazy(() => import('./pages/payment/index'));
 const DynamicProductsIndex = React.lazy(() => import('./pages/products/index'));
 const DynamicProductsId = React.lazy(() => import('./pages/products/[id]'));
 
@@ -13,6 +15,8 @@ export const routes = [
     element: <GlobalLayout />,
     children: [
       { path: '/', element: <DynamicIndex />, index: true},
+      { path: '/cart', element: <DynamicCartIndex />, index: true},
+      { path: '/payment', element: <DynamicPaymentIndex />, index: true},
       { path: '/products', element: <DynamicProductsIndex />, index: true},
       { path: '/products/:id', element: <DynamicProductsId />, },
     ]
@@ -21,6 +25,8 @@ export const routes = [
 
 export const pages = [
   { route: '/' },
+  { route: '/cart' },
+  { route: '/payment' },
   { route: '/products' },
   { route: '/products/:id' },
 ]
